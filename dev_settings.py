801c1785e314838b083d2e91a7c74f50c8509be5
NgwaNathan <nathanngwa3@gmail.com>
2026-05-07 17:37:42 +0100

    fix missing urls in the admin

--- dev_settings.py
+++ dev_settings.py
@@ -1,13 +1,42 @@
 INSTALLED_APPS = [
+    "django.contrib.admin",
     "django.contrib.contenttypes",
     "django.contrib.auth",
+    "django.contrib.messages",
+    "django.contrib.sessions",
+    "django.contrib.staticfiles",
     "kunshort_payment",
+]
+
+STATIC_URL = "/static/"
+
+ROOT_URLCONF = "dev_urls"
+
+MIDDLEWARE = [
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+]
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    }
 ]
 
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": ":memory:",
+        "NAME": "dev.sqlite3",
     }
 }
 
@@ -46,3 +75,7 @@ MTN_DISBURSEMENT = {
 FLUTTERWAVE_PAYMENT = {
     "SECRET_KEY": "fake-flutterwave-key",
 }
+
+DEBUG = True
+
+SECRET_KEY = "dev-only-insecure-secret-key-do-not-use-in-production"
